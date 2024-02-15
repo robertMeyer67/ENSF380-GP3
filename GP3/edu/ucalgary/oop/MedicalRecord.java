53e22199e52db30ccb7da5311c705ce9dfe26ef7
brandonnguyen3 <brandon.nguyen3@ucalgary.ca>
2024-02-15 09:31:45 -0700

    Changed MedicalRecord so that it now throws an exception if there's an incorrect date. Have not tested it yet.

--- GP3/edu/ucalgary/oop/MedicalRecord.java
+++ GP3/edu/ucalgary/oop/MedicalRecord.java
@@ -1,10 +1,15 @@
 package edu.ucalgary.oop;
 
+import java.util.regex.*;
+
 public class MedicalRecord {
 	private Location location;
 	private String treatmentDetails;
 	private String dateOfTreatment;
-	
+
+	private static final String REGEX = "^\\d{4}-\\d{2}-\\d{2}$";
+    private static final Pattern PATTERN = Pattern.compile(REGEX);
+
 	//constructor
 	public MedicalRecord(Location location, String treatmentDetails, String dateOfTreatment) {
 		this.location = location;
@@ -33,10 +38,13 @@ public class MedicalRecord {
 		return dateOfTreatment;
 	}
 	public void setDateOfTreatment(String dateOfTreatment) {
-		//insert code here for throwing an exception
+
+		// Validate date format using regex pattern
+        Matcher matcher = PATTERN.matcher(dateOfTreatment);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException("Invalid date format. Please use yyyy-MM-dd format.");
+        }
 		this.dateOfTreatment = dateOfTreatment;
 	}
 	
-	
-
 }
