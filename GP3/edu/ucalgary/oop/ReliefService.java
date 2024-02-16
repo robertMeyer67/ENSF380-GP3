d2f68796ccc85ff93a2cf0925e764a55a4ab0edd
brandonnguyen3 <brandon.nguyen3@ucalgary.ca>
2024-02-15 20:27:15 -0700

    Added throw exception for ReliefService. Not yet tested.

--- GP3/edu/ucalgary/oop/ReliefService.java
+++ GP3/edu/ucalgary/oop/ReliefService.java
@@ -1,4 +1,5 @@
 package edu.ucalgary.oop;
+import java.util.regex.*;
 
 public class ReliefService {
     
@@ -8,12 +9,14 @@ public class ReliefService {
     private String dateOfInquiry;
     private String infoProvided;
     
+    private static final String REGEX = "^\\d{4}-\\d{2}-\\d{2}$";
+    private static final Pattern PATTERN = Pattern.compile(REGEX);
 
     ReliefService(Inquirer inquirer, DisasterVictim missingPerson, String dateOfInquiry,String infoProvided, Location lastKnownLocation){
 
         this.inquirer = inquirer;
         this.missingPerson = missingPerson;
-        this.dateOfInquiry = dateOfInquiry;
+        setDateOfInquiry(dateOfInquiry);
         this.infoProvided = infoProvided;
         this.lastKnownLocation = lastKnownLocation;
         
@@ -30,6 +33,11 @@ public class ReliefService {
     }
     
     public void setDateOfInquiry(String dateOfInquiry){
+        // Validate date format using regex pattern
+        Matcher matcher = PATTERN.matcher(dateOfInquiry);
+        if (!matcher.matches()) {
+                throw new IllegalArgumentException("Invalid date format. Please use yyyy-MM-dd format.");
+            }
         this.dateOfInquiry = dateOfInquiry;
     }
     public String getInfoProvided(){
@@ -41,7 +49,4 @@ public class ReliefService {
     public String getLogDetails(){
         return "Inquirer:" + this.inquirer.getFirstName() + " " + this.inquirer.getLastName() + ", Missing Person:"+ this.missingPerson.getFirstName() + " " + this.missingPerson.getLastName() + ", Date Of Inquiry:"+ this.dateOfInquiry + ", Info Provided: " + this.infoProvided + ", Last Known Location: " + this.lastKnownLocation.getName();
     }
-
-
-
 }
